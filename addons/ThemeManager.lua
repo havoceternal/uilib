9f9f0622975eff6d2156b8ba421dd164bc06e05f
havoceternal <ambiiswet@gmail.com>
2026-09-15 20:19:00 +1000

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -14,7 +14,8 @@ local ThemeManager = {} do
 		['Ubuntu'] 			= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
 		['Quartz'] 			= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
 		['Havoc'] 			= { 9, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"686868","BackgroundColor":"000000","OutlineColor":"353535"}') },
-	}
+        ['Solitaire'] 		= { 10, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"0c0c0c","AccentColor":"f369ff","BackgroundColor":"000000","OutlineColor":"000000"}') },
+    }
 
 	function ThemeManager:ApplyTheme(theme)
 		local customThemeData = self:GetCustomTheme(theme)
